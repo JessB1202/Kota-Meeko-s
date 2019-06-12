fdc0b4dc04a49828f0bb91ed9c426372e494eebe
Campbell <mikecampbell@quickenloans.com>
2019-06-12 19:25:58 -0400

    added click handling for accessories (link and category)

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const foodCategory = document.getElementById("foodCategory");
 const toysCategory = document.getElementById("toysCategory");
 const cleaningCategory = document.getElementById("cleaningCategory");
+const accessoriesCategory = document.getElementById("accessoriesCategory");
 
 const foodLink = document.getElementById("foodLink");
 const toysLink = document.getElementById("toysLink");
 const cleaningLink = document.getElementById("cleaningLink");
+const accessoriesLink = document.getElementById("accessoriesLink");
 
 foodLink.addEventListener('click', function(){
-    foodCategory.scrollTo(0,0);
+    window.scrollTo(0,0);
 });
 
 toysLink.addEventListener('click', function(){
@@ -16,4 +18,9 @@ toysLink.addEventListener('click', function(){
 
 cleaningLink.addEventListener('click', function(){
     cleaningCategory.scrollIntoView();
+});
+
+accessoriesLink.addEventListener('click', function(){
+    accessoriesCategory.scrollIntoView();
+
 });
